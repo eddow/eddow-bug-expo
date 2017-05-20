8aaa43ee71c698d709589edde8243408d91f9273
fmdm <eeddow@protonmail.com>
2017-05-20 17:34:52 +0300

    +generic fuse

---
+++ fuse.js
@@ -0,0 +1,14 @@
+const {FuseBox, VuePlugin} = require("fuse-box");
+const fuse = FuseBox.init({
+	homeDir: "src",
+	output: "dist/$name.js",
+	plugins: [
+		VuePlugin()
+	],
+	cache: false/*,
+	globals: {default: '*'}*/
+});
+fuse.bundle("bndl")
+	.instructions('> index.ts');
+
+fuse.run();
