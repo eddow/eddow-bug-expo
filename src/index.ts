8f3ee0e1dc3c2eb626338a48aa47b06ea1b1048e
fmdm <eeddow@gmail.com>
2018-12-10 17:18:05 +0200

    implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import vue from 'Vue'
+import Component from 'vue-class-component'
 
-export default {};
+class aComponent extends Vue {
+
+}
+
+@Component
+export default class myComponent extends aComponent {
+	static callMe(name: string): string { return `Good job ${name}!`; }
+}
+
+console.log(myComponent.callMe('Robert Paulson'));
